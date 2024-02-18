ac6d314cc9f25495dd9ac2aab0c14c2ac2c6d231
Vivian Bui <vivianbui@live.ca>
2024-02-18 15:47:12 -0500

    corrections + sliding bg

--- script.js
+++ script.js
@@ -308,22 +308,22 @@ $(document).ready(function () {
   );
 });
 
-// let slideIndex = 0;
-// showSlides();
+let slideIndex = 0;
+showSlides();
 
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {
-//     slideIndex = 1;
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-//   setTimeout(showSlides, 5000); // Change image every 2 seconds
-// }
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4200); // Change image every X seconds
+}
 
 function scrollFunction() {
   const element = document.getElementById("scroll");
